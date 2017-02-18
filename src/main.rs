402d8e53bdd45afaf9fad2e364dc9efb6e0955cd
Adam Faur <adam.faur@gmail.com>
2017-02-17 19:46:13 -0800

    Handle all icns sizes

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,9 @@ use icns::{IconFamily, IconType};
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
 
-fn write_icon(path: String, icon: String, save_as: String) -> bool {
+fn icon_to_png(source: &str, output: &str) -> bool {
   // Read binary data in to a buffer
-  let file = BufReader::new(
-    File::open(path.clone() + icon.as_str().clone())
-    .unwrap()
-  );
+  let file = BufReader::new( File::open(source).unwrap() );
 
   // Load an icon family from an ICNS file.
   let icon_family = IconFamily::read(file).unwrap();
@@ -22,31 +19,30 @@ fn write_icon(path: String, icon: String, save_as: String) -> bool {
   ];
 
   // Get the best quality icon
-  for (iter_num, &icon_format) in types.iter().enumerate() {
+  for (_, &icon_format) in types.iter().enumerate() {
     // TODO: Refactor so that if the first item in the quality levels is
     // not found that we try the next best quality until we get something.
-    let default_icon_image = icon_family.get_icon_with_type(icon_format).unwrap();
+    let icon = icon_family.get_icon_with_type(icon_format);
+    match icon {
+        Ok(default_icon_image) => {
+          // Create a png from the best quality icon
+          let default_icon_file = BufWriter::new( File::create(output).unwrap() );
 
-    // Create a png from the best quality icon
-    let default_icon_file = BufWriter::new(
-      File::create(save_as.to_string()).unwrap()
-    );
+          // Save the file locally
+          default_icon_image.write_png(default_icon_file).unwrap();
+          break;
+        },
+        Err(_) => continue,
+    }
 
-    // Save the file locally
-    default_icon_image.write_png(
-      default_icon_file
-    ).unwrap();
   }
-
-
   return true;
 }
 
 fn main() {
   // Write the system icon
-  write_icon(
-    "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/".to_string(),
-    "GenericApplicationIcon.icns".to_string(),
-    "defaultIcon.png".to_string()
+  icon_to_png(
+    "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/GenericApplicationIcon.icns",
+    "defaultIcon.png"
   );
 }
